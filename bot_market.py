c3f5480ad82d676864ce857a086144fe5a24e708
Aspick123 <105391216+Aspick123@users.noreply.github.com>
2026-06-09 14:13:37 +0100

    Add files via upload

--- bot_market.py
+++ bot_market.py
@@ -14,6 +14,24 @@
 ║  python bot_market.py                                        ║
 ╚══════════════════════════════════════════════════════════════╝
 """
+from threading import Thread
+from flask import Flask
+
+app = Flask("")
+
+@app.route("/")
+def home():
+    return "Le bot Marketplace est en ligne !"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
 
 import json, os, time, logging, datetime
 from collections import defaultdict
